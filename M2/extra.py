8bab3d05f5b896455ac2720dedfb0166328939da
Jakub Tadych <jakub.tadych@team.blue>
2025-03-18 21:25:33 +0000

    extra challange

--- M2/extra.py
+++ M2/extra.py
@@ -1,3 +1,5 @@
+print('---Zadanie 1---')
+
 exam_points = {
   "Mariusz":30,
   "Mateusz":55,
@@ -16,7 +18,6 @@ exam_points = {
 
 failed_students = []
 top_students = []
-
 max_score = 0
 student = None
 for name, points in exam_points.items():
@@ -34,3 +35,57 @@ print("Failed students: {}" .format(failed_students))
 print("Top students: {}" .format(top_students))
 best_student = (student, max_score)
 print('{}' .format(best_student))
+
+
+print('---Zadanie 2---')
+names = ['Paweł', 'Kewin', 'Ireneusz', 'Bolesław', 'Mateusz', 'Edward', 'Piotr', 'Jan', 'Denis', 'Amir',
+'Igor', 'Borys', 'Robert', 'Ariel', 'Kuba', 'Rafał', 'Mateusz', 'Emanuel', 'Alfred', 'Artur', 'Jakub',
+'Ludwik', 'Bolesław', 'Remigiusz', 'Martin', 'Dobromił', 'Mariusz', 'Amadeusz', 'Łukasz', 'Bolesław', 'Amir',
+'Artur', 'Albert', 'Olgierd', 'Alek', 'Kordian', 'Julian', 'Anastazy', 'Emanuel', 'Józef']
+
+name_dict = {}
+for name in names:
+  first_letter = name[0]
+
+  if first_letter in name_dict:
+    if name not in name_dict[first_letter]:
+      name_dict[first_letter].append(name)
+  else:
+    name_dict[first_letter] = [name]
+
+print("Name Dict: {}" .format(name_dict))
+
+
+print('---Zadanie 3---')
+
+i = 1
+num = 30
+fibonacci = [1]
+while True:
+  if i == num:
+    break
+  fibonacci.append(sum(fibonacci[-2:]))
+  i+=1
+
+print(fibonacci)
+
+
+print('---Zadanie 4--')
+
+a = 1
+b = -2
+c = -5
+
+def equation(a,b,c):
+  delta = (b**2) -4*a*c
+  if delta > 0:
+    x1 = b*(-1) - delta**(0.5)/2*a
+    x2 = b*(-1) + delta**(0.5)/2*a
+    return x1, x2
+  elif delta == 0:
+    return -b/(2*a)
+  else:
+    return 'Brak rozwiązań'
+
+
+print(equation(a, b, c))

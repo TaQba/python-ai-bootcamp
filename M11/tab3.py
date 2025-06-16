b3e9c489475722f74a6ed1b86e015400f5f67f69
Jakub Tadych <jakub.tadych@team.blue>
2025-06-16 14:14:10 +0100

    Modul11 zadanie 2

--- M11/tab3.py
+++ M11/tab3.py
@@ -1,21 +1,41 @@
-
+import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
+import plotly.express as px
 
 def render_tab(df):
+    df['weekday'] = df['tran_date'].dt.day_name()
+    df = df.drop('tran_date', axis=1)
+    all = df.groupby(['weekday']).sum().sort_values(by='total_amt', ascending=False).reset_index()
+    fig = go.Figure(data=[go.Bar(x=all['weekday'], y=all['total_amt'])],layout=go.Layout(title='Wszystkie kanały na dzień tygodnia'))
 
-    grouped = df[df['total_amt']>0].groupby('prod_cat')['total_amt'].sum()
-    fig = go.Figure(data=[go.Pie(labels=grouped.index,values=grouped.values)],layout=go.Layout(title='Udział grup produktów w sprzedaży'))
+    ind = df.groupby(['Store_type', 'weekday']).sum().reset_index()
 
-    layout = html.Div([html.H1('Produkty',style={'text-align':'center'}),
+    df2 = pd.DataFrame({
+        "Day": [day for day in ind['weekday']],
+        "Amount": [amount for amount in ind['total_amt']],
+        "Store Type": [types for types in ind['Store_type']],
+    })
 
-                        html.Div([html.Div([dcc.Graph(id='pie-prod-cat',figure=fig)],style={'width':'50%'}),
-                        html.Div([dcc.Dropdown(id='prod_dropdown',
-                                    options=[{'label':prod_cat,'value':prod_cat} for prod_cat in df['prod_cat'].unique()],
-                                    value=df['prod_cat'].unique()[0]),
-                                    dcc.Graph(id='barh-prod-subcat')],style={'width':'50%'})],style={'display':'flex'}),
-                                    html.Div(id='temp-out')
-                        ])
+    fig2 = px.bar(df2, x="Day", y="Amount", color="Store Type", barmode="group")
+
+    layout = html.Div(
+        [
+            html.H1('Kanał sprzedaży',style={'text-align':'center'}),
+            html.Div(
+            [
+                        html.Div(
+                            [dcc.Graph(id='bar-weekdays-all',figure=fig)],style={'width':'50%'}
+                        ),
+                        html.Div(
+                            [dcc.Graph(id='bar-weekdays-ind', figure=fig2)], style={'width': '50%'}
+                        ),
+                    ],
+                style={'display':'flex'}
+            ),
+            html.Div(id='temp-out')
+        ]
+    )
 
     return layout
